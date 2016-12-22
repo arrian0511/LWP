d481f68cb5ea7693879e90d682e84d1da9db61e4
arrian0511 <a_pascval@yahoo.com>
2016-12-23 00:31:19 +0800

    ADD: Setting File

---
+++ src/Twt.Lwp.Models/src/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Twt.Lwp.Models
+{
+	public class User
+	{
+		public User () {
+			this.Name		= string.Empty;
+			this.Tweets		= new List<Tweet> ();
+		}
+
+		public string				Name	{ get; set; }
+		public ICollection<Tweet>	Tweets	{ get; set; }
+	}
+}
---
+++ src/Twt.Lwp.Web/Configuration/TwitterAuth.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Twt.Lwp.Web.Configuration
+{
+	public class TwitterAuth
+	{
+		public string ConsumerKey { get; set; }
+		public string ConsumerSecret { get; set; }
+		public string OAuthToken { get; set; }
+		public string OAuthTokenSecret { get; set; }
+	}
+}
